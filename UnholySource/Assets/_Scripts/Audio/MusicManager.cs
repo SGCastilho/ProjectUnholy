289533b11f4c40e30d7661f519f7d538e10a50f3
Sergio Guilherme Castilho <65431005+SGCastilho@users.noreply.github.com>
2024-11-23 16:00:25 -0300

    Background music added

---
+++ UnholySource/Assets/_Scripts/Audio/MusicManager.cs
@@ -0,0 +1,100 @@
+using DG.Tweening;
+using UnityEngine;
+
+namespace Core.Audio
+{
+    public sealed class MusicManager : MonoBehaviour
+    {
+        [Header("Classes")]
+        [SerializeField] private AudioSource audioSource;
+
+        [Header("Settings")]
+        [SerializeField] private bool inTransition;
+        [SerializeField] private bool predatorChasing;
+        [SerializeField] [Range(0.1f, 4f)] private float fadeDuration = 1f;
+
+        private float _maxVolume;
+
+        private void OnEnable() 
+        {
+            if(audioSource == null) return;
+
+            _maxVolume = audioSource.volume;
+        }
+
+        public void PlayAudio(AudioClip musicAudio)
+        {
+            if(inTransition || predatorChasing || audioSource.clip == musicAudio) return;
+
+            audioSource.clip = musicAudio;
+            audioSource.volume = _maxVolume;
+
+            audioSource.Play();
+        }
+
+        public void PlayAudioFadeIn(AudioClip musicAudio)
+        {
+            if(inTransition || predatorChasing || audioSource.clip == musicAudio) return;
+
+            audioSource.clip = musicAudio;
+            audioSource.volume = 0f;
+
+            audioSource.Play();
+
+            audioSource.DOKill();
+
+            inTransition = true;
+
+            audioSource.DOFade(_maxVolume, fadeDuration).OnComplete(() => { inTransition = false; });
+        }
+
+        public void TransitateTo(AudioClip musicAudio)
+        {
+            if(inTransition || predatorChasing) return;
+
+            audioSource.DOKill();
+
+            inTransition = true;
+
+            audioSource.DOFade(0f, fadeDuration).OnComplete(() => 
+            {
+                audioSource.Stop();
+
+                audioSource.clip = musicAudio;
+                audioSource.volume = 0f;
+
+                audioSource.Play();
+
+                audioSource.DOKill();
+                audioSource.DOFade(_maxVolume, fadeDuration).OnComplete(() => { inTransition = false; });
+            }).SetUpdate(true);
+        }
+
+        public void StopAudio()
+        {
+            if(inTransition || predatorChasing) return;
+
+            audioSource.Stop();
+
+            audioSource.clip = null;
+        }
+
+        public void StopAudioFadeOut()
+        {
+            if(inTransition || predatorChasing) return;
+
+            audioSource.DOKill();
+
+            inTransition = true;
+
+            audioSource.DOFade(0f, fadeDuration).OnComplete(() => 
+            {
+                audioSource.Stop();
+                
+                inTransition = false;
+
+                audioSource.clip = null;
+            }).SetUpdate(true);
+        }
+    }
+}
