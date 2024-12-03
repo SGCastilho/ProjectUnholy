1a3a191001ede8f2bc5ba00e432077a6e105fede
Sergio Guilherme Castilho <65431005+SGCastilho@users.noreply.github.com>
2024-12-03 17:12:30 -0300

    Item Notification finish

--- UnholySource/Assets/_Scripts/UI/Gameplay/Player HUD/UIItemNotificantion.cs
+++ UnholySource/Assets/_Scripts/UI/Gameplay/Player HUD/UIItemNotificantion.cs
@@ -23,7 +23,14 @@ namespace Core.UI
         #endregion
 
         [Header("Classes")]
-        [SerializeField] private CanvasGroup canvasGroup;
+        [SerializeField] private CanvasGroup backgroundCanvasGroup;
+        [SerializeField] private CanvasGroup windowNameCanvasGroup;
+
+        [Space(6)]
+
+        [SerializeField] private CanvasGroup itemNameCanvasGroup;
+        [SerializeField] private CanvasGroup itemSpriteCanvasGroup;
+        [SerializeField] private CanvasGroup closeButtonCanvasGroup;
 
         [Space(10)]
 
@@ -32,6 +39,27 @@ namespace Core.UI
 
         [Header("Settings")]
         [SerializeField] [Range(0.1f, 1f)] private float fadeDuration = 0.2f;
+        [SerializeField] [Range(0.1f, 1f)] private float contentFadeDuration = 0.2f;
+
+        [Space(10)]
+
+        [SerializeField] private float windowMovement = 40f;
+        [SerializeField] private float itemNameMovement = 340f;
+
+        private RectTransform windowNameRect;
+        private RectTransform itemNameRect;
+        private RectTransform itemSpriteRect;
+        private RectTransform closeButtonRect;
+
+        private void Awake() => CacheVariables();
+
+        private void CacheVariables()
+        {
+            windowNameRect = windowNameCanvasGroup.GetComponent<RectTransform>();
+            itemNameRect = itemNameCanvasGroup.GetComponent<RectTransform>();
+            itemSpriteRect = itemSpriteCanvasGroup.GetComponent<RectTransform>();
+            closeButtonRect = closeButtonCanvasGroup.GetComponent<RectTransform>();
+        }
 
         public void ShowNotification(ItemData itemToShow)
         {
@@ -41,12 +69,62 @@ namespace Core.UI
             OnBlockControls?.Invoke();
             OnShowInterface?.Invoke();
 
-            canvasGroup.DOKill();
-            canvasGroup.DOFade(1f, fadeDuration).OnComplete(() => 
+            backgroundCanvasGroup.DOKill();
+            backgroundCanvasGroup.DOFade(1f, fadeDuration).OnComplete(() => 
             {
-                canvasGroup.interactable = true; 
-                canvasGroup.blocksRaycasts = true;  
+                backgroundCanvasGroup.interactable = true; 
+                backgroundCanvasGroup.blocksRaycasts = true;
+
+                ShowWindowName();
             }).SetUpdate(true);
+        }
+
+        private void ShowWindowName()
+        {
+            windowNameCanvasGroup.alpha = 0f;
+            windowNameRect.anchoredPosition = new Vector2(-windowMovement, windowNameRect.anchoredPosition.y);
+
+            windowNameCanvasGroup.DOKill();
+            windowNameRect.DOKill();
+
+            windowNameCanvasGroup.DOFade(1f, fadeDuration).SetUpdate(true);
+            windowNameRect.DOAnchorPosX(0f, fadeDuration).OnComplete(() => { ShowItemSprite(); }).SetUpdate(true);
+        }
+
+        private void ShowItemSprite()
+        {
+            itemSpriteCanvasGroup.alpha = 0f;
+            itemSpriteRect.anchoredPosition = new Vector2(itemSpriteRect.anchoredPosition.x, -windowMovement);
+
+            itemSpriteCanvasGroup.DOKill();
+            itemSpriteRect.DOKill();
+
+            itemSpriteCanvasGroup.DOFade(1f, contentFadeDuration).SetUpdate(true);
+            itemSpriteRect.DOAnchorPosY(0f, contentFadeDuration).OnComplete(() => { ShowItemName(); }).SetUpdate(true);
+        }
+
+        private void ShowItemName()
+        {
+            itemNameCanvasGroup.alpha = 0f;
+            itemNameRect.anchoredPosition = new Vector2(itemNameRect.anchoredPosition.x, itemNameMovement);
+
+            itemNameCanvasGroup.DOKill();
+            itemNameRect.DOKill();
+
+            itemNameCanvasGroup.DOFade(1f, contentFadeDuration).SetUpdate(true);
+            itemNameRect.DOAnchorPosY(300f, contentFadeDuration).OnComplete(() => { ShowCloseButton(); }).SetUpdate(true);
+        }
+
+        private void ShowCloseButton()
+        {
+            closeButtonCanvasGroup.alpha = 0f;
+            closeButtonRect.anchoredPosition = new Vector2(closeButtonRect.anchoredPosition.x, -itemNameMovement);
+
+            closeButtonCanvasGroup.DOKill();
+            closeButtonRect.DOKill();
+
+            closeButtonCanvasGroup.DOFade(1f, contentFadeDuration).SetUpdate(true);
+            closeButtonRect.DOAnchorPosY(-320f, contentFadeDuration).SetUpdate(true);
         }
 
         public void HideNotification()
@@ -54,12 +132,41 @@ namespace Core.UI
             OnAllowControls?.Invoke();
             OnHideInteface?.Invoke();
 
-            canvasGroup.DOKill();
-            canvasGroup.DOFade(0f, fadeDuration).OnComplete(() => 
+            backgroundCanvasGroup.DOKill();
+            backgroundCanvasGroup.DOFade(0f, fadeDuration).OnComplete(() => 
             {
-                canvasGroup.interactable = false; 
-                canvasGroup.blocksRaycasts = false;  
+                HideWindowName();
+                HideItemSprite();
+                HideItemName();
+                HideCloseButton();
+
+                backgroundCanvasGroup.interactable = false; 
+                backgroundCanvasGroup.blocksRaycasts = false; 
             }).SetUpdate(true);
+        }
+
+        private void HideWindowName()
+        {
+            windowNameCanvasGroup.alpha = 0f;
+            windowNameRect.anchoredPosition = new Vector2(-windowMovement, windowNameRect.anchoredPosition.y);
+        }
+
+        private void HideItemSprite()
+        {
+            itemSpriteCanvasGroup.alpha = 0f;
+            itemSpriteRect.anchoredPosition = new Vector2(itemSpriteRect.anchoredPosition.x, -windowMovement);
+        }
+
+        private void HideItemName()
+        {
+            itemNameCanvasGroup.alpha = 0f;
+            itemNameRect.anchoredPosition = new Vector2(itemNameRect.anchoredPosition.x, itemNameMovement);
+        }
+
+        private void HideCloseButton()
+        {
+            closeButtonCanvasGroup.alpha = 0f;
+            closeButtonRect.anchoredPosition = new Vector2(closeButtonRect.anchoredPosition.x, -itemNameMovement);
         }
     }
 }
