0d30e648b0835e953af204b7a012c773509f99ca
Sergio Guilherme Castilho <65431005+SGCastilho@users.noreply.github.com>
2025-01-29 17:10:22 -0300

    Player Thinking UI added

---
+++ UnholySource/Assets/_Scripts/UI/Gameplay/Player HUD/UIThikingDialogue.cs
@@ -0,0 +1,94 @@
+using TMPro;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Core.UI
+{
+    public sealed class UIThikingDialogue : MonoBehaviour
+    {
+        [Header("Classes")]
+        [SerializeField] private CanvasGroup canvasGroup;
+
+        [Space(10)]
+
+        [SerializeField] private TextMeshProUGUI messageTMP;
+
+        [Header("Settings")]
+        [SerializeField] private bool emitingMessage;
+        [SerializeField] [Range(0.1f, 1f)] private float fadeDuration = 0.2f;
+
+        [Space(10)]
+
+        [SerializeField] [Range(1f , 4f)] private float messageDuration = 1.6f;
+
+        private float _currentMessageDuration;
+        private string _lastMessage;
+
+        //TROCAR PARA UM SCRIPTABLE OBJECT
+        public void EmitMessage(string message)
+        {
+            if(message == null || message == string.Empty || _lastMessage == message) return;
+
+            if(emitingMessage)
+            {
+                ResetMessage(message);
+            }
+            else
+            {
+                _lastMessage = message;
+
+                messageTMP.text = message;
+                emitingMessage = true;
+
+                FadeIn();
+            }
+        }
+
+        private void Update() 
+        {
+            if(emitingMessage)
+            {
+                _currentMessageDuration += Time.deltaTime;
+                if(_currentMessageDuration >= messageDuration)
+                {
+                    emitingMessage = false;
+                    _currentMessageDuration = 0f;
+                    
+                    FadeOut();
+                }
+            }
+        }
+
+        private void FadeIn()
+        {
+            canvasGroup.DOKill();
+            canvasGroup.DOFade(1f, fadeDuration);
+        }
+
+        private void FadeOut()
+        {
+            canvasGroup.DOKill();
+            canvasGroup.DOFade(0f, fadeDuration).OnComplete(() => 
+            {                 
+                _lastMessage = string.Empty;
+                messageTMP.text = string.Empty;
+            });
+        }
+
+        private void ResetMessage(string newMessage)
+        {
+            emitingMessage = false;
+
+            canvasGroup.DOKill();
+            canvasGroup.DOFade(0f, fadeDuration).OnComplete(() => 
+            { 
+                messageTMP.text = newMessage;
+
+                emitingMessage = true;
+                _currentMessageDuration = 0f;
+
+                FadeIn();
+            });
+        }
+    }
+}
